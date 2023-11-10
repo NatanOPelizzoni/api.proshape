0a893dff1139102c5feb09b079f3d7da99701624
Natan O Pelizzoni <natan2624@gmail.com>
2023-11-10 02:50:32 -0300

    refactor: Routes refactored

--- routes/api.php
+++ routes/api.php
@@ -7,18 +7,9 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-Route::group(['prefix' => 'user'], static function(){
+Route::group(['middleware' => 'api', 'prefix' => 'auth'], static function(){
     Route::post('register','App\Http\Controllers\AuthController@register');
     Route::post('login', 'App\Http\Controllers\AuthController@login');
 });
