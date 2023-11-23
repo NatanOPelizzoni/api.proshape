69d532c0a31805bd5f58159639df76585f42ef97
Natan O Pelizzoni <natan2624@gmail.com>
2023-11-23 00:17:08 -0300

    feat: Student model, migration and controller

---
+++ database/migrations/2023_11_23_003825_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('birth_date');
+            $table->float('weight');
+            $table->float('height');
+            $table->string('training_goals');
+            $table->integer('training_frequency');
+            $table->string('health_restrictions');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
